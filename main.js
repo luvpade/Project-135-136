2265ae9b36a52af190c1e36f36970e4107e5c405
DamDakshDam <81851219+luvpade@users.noreply.github.com>
2022-02-11 21:04:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+video = "";
+objects =[];
+status = "";
+
+
+
+function setup() {
+    canvas = createCanvas(480, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    video.size(480, 380)
+}
+
+function draw() {
+    image(video ,0, 0, 480, 380);
+}
+
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
